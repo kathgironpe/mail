5e30a83ccdef11582bc952d2c71dc6806e70749a
Katherine Giron Pe <katherinepe@me.com>
2022-06-20 10:08:27 +0800

    Test the GET campaign API Ensure all required keys are returned for response body

--- spec/controllers/api/campaigns_controller_spec.rb
+++ spec/controllers/api/campaigns_controller_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 describe Api::CampaignsController, type: :controller do
   describe 'GET show' do
-    let!(:mailing) { create(:mailing) }
+    let!(:store) { create(:store) }
+    let!(:subscriber) { create(:subscriber, store: store) }
+    let!(:mailing) { create(:mailing, store: store) }
 
     it 'returns a status 200' do
       get :show, params: { id: mailing.id }
 
+      hash_body = JSON.parse(response.body)
+
       expect(response).to have_http_status(:ok)
-      expect(response.body).to eq('{}')
+      expect(hash_body.keys).to match_array(%w[from html recipient_variables])
     end
   end
 end
